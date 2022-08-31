9251e0d66cfba5434ae3ef2270a30eeb9f1dbadc
unknown <kanbayoguzhan@gmail.com>
2022-08-31 12:22:28 +0300

    changes

--- src/main/java/com/sendpost/model/UserModel.java
+++ src/main/java/com/sendpost/model/UserModel.java
@@ -4,12 +4,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.NonNull;
 
 @Data
 @Entity
@@ -18,8 +17,21 @@ public class UserModel {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
+	
+	
+	@Size(min = 4,max =224)
+	@NonNull
 	private String userName;
+	
+	
+	@Size(min = 4,max =224)
+	@NonNull
 	private String displayName;
+	
+	
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}")
+	@NonNull
 	private String password;
+	
 
 }
